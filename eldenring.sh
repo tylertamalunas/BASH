1f4eb00496b61a9f7fe3444f5cf2f666c1ff9169
tylertamalunas <tylertamalunas@gmail.com>
2022-05-30 20:11:44 -0500

    updated script

--- eldenring.sh
+++ eldenring.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
+# start of learning conditionals in BASH 
+
 echo "You Died"
+
+
+
+#first battle
+
+#random number of 0 or 1
+beast=$(( $RANDOM % 2 ))
+
+echo "The first battle! Pick a number, 0 or 1 and attempt to beat your foe!"
+
+read tarnished
+
+if [[ $beast == $tarnished ]]; then
+	echo "Enemy vanquished! Nice job tarnished"
+else 
+	echo "You Died"
+fi
