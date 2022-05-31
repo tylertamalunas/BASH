f634e589b1b1c48c42ea974935fc2fb8b5bc1a9b
tylertamalunas <tylertamalunas@gmail.com>
2022-05-30 20:35:16 -0500

    added more conditions

--- eldenring.sh
+++ eldenring.sh
@@ -15,8 +15,27 @@ echo "The first battle! Pick a number, 0 or 1 and attempt to beat your foe!"
 
 read tarnished
 
-if [[ $beast == $tarnished ]]; then
+if [[ $beast == $tarnished && $tarnished == 0 ]]; then
 	echo "Enemy vanquished! Nice job tarnished"
 else 
 	echo "You Died"
+	exit 1
+fi
+
+
+sleep 2
+
+
+#Now we face a harder enemy, Margit
+margit=$(( $RANDOM % 10 ))
+
+echo "Margit: You dare try to pass foul tarnished?"
+echo "Pick a number from 0 to 9 and try to defeat Margit the Fell Omen"
+
+read foul_tarnished
+
+if [[ $margit == $foul_tarnished || $foul_tarnished == "magic" ]]; then
+	echo "I shall remember thee foul tarnished...I will fell you next time."
+else 
+	echo "Put these foolish ambitions to rest...You Died"
 fi
